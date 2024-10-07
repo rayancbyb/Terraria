98bee779b96e735aa0b8f7b9eb9700b692d179e7
rayancbyb <rayancbyb@gmail.com>
2024-10-07 16:16:44 +0200

    develop2

--- src/terraria/Terraria.java
+++ src/terraria/Terraria.java
@@ -8,7 +8,7 @@ public class Terraria {
         
         // Sumar los números y mostrar el resultado
         int resultado = cubo.sumar();
-        System.out.println(" suma de los números hola " + resultado);
+        System.out.println(" suma de los números adios " + resultado);
     }
     
 }
