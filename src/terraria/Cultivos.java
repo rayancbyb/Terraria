bd4366e25629c7d3985192b61bbe398835970406
rayancbyb <rayancbyb@gmail.com>
2024-10-21 14:07:51 +0200

    clase cultivos

---
+++ src/terraria/Cultivos.java
@@ -0,0 +1,14 @@
+package terraria;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author rayanlinux
+ */
+public enum Cultivos {
+    
+}
