d59529bddff72a416d488aa0401b13f5d6de2d02
rayancbyb <rayancbyb@gmail.com>
2024-10-22 20:20:52 +0200

    cambio

--- src/terraria/Lienzo.java
+++ src/terraria/Lienzo.java
@@ -17,6 +17,7 @@ public class Lienzo extends Canvas implements KeyListener {
     private Image fondo;
     private Bloque[] suelo;
     private Bloque[] troncos;
+    private Bloque[] hojas;
 
     public Lienzo(Personaje personaje) {
         this.personaje = personaje;
@@ -30,7 +31,7 @@ public class Lienzo extends Canvas implements KeyListener {
         addKeyListener(this);
         setFocusable(true);
         crearSuelo();
-        crearTroncos();
+        crearTroncosYHojas();
     }
 
     private void crearSuelo() {
@@ -43,45 +44,58 @@ public class Lienzo extends Canvas implements KeyListener {
         }
     }
 
-    private void crearTroncos() {
-        int numPilas = 5; // Número de pilas de troncos
+    private void crearTroncosYHojas() {
+        int numPilas = 2; // Número de pilas de troncos
         int troncosPorPila = 3; // Número de troncos por pila
         troncos = new Bloque[numPilas * troncosPorPila];
+        hojas = new Bloque[numPilas * 3]; // Añadir espacio para las hojas: una encima y dos a los lados
         int altoBloque = 50;
+        
         for (int i = 0; i < numPilas; i++) {
-            int x = i * altoBloque * 3; // Espaciado entre pilas
+            int x = i * altoBloque * 5; // Espaciado entre pilas
             for (int j = 0; j < troncosPorPila; j++) {
                 troncos[i * troncosPorPila + j] = new Tronco(x, 550 - (j + 1) * altoBloque, altoBloque);
             }
+            // Añadir hoja encima del último tronco
+            hojas[i * 3] = new Hojas(x, 550 - (troncosPorPila + 1) * altoBloque, altoBloque);
+            // Añadir hojas a los lados del último tronco
+            hojas[i * 3 + 1] = new Hojas(x - altoBloque, 550 - (troncosPorPila ) * altoBloque, altoBloque);
+            hojas[i * 3 + 2] = new Hojas(x + altoBloque, 550 - (troncosPorPila ) * altoBloque, altoBloque);
         }
     }
+    
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        if (fondo != null) {
-            g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
-        }
-        for (Bloque bloque : suelo) {
-            bloque.dibujar(g);
-        }
-        for (Bloque tronco : troncos) {
-            tronco.dibujar(g);
-        }
-        if (personaje.getDireccion() == 'd' && imagenDerecha != null) {
-            g.drawImage(imagenDerecha, personaje.getX(), personaje.getY(), 50, 50, this);
-        } else if (personaje.getDireccion() == 'a' && imagenIzquierda != null) {
-            g.drawImage(imagenIzquierda, personaje.getX(), personaje.getY(), 50, 50, this);
-        } else {
-            g.fillRect(personaje.getX(), personaje.getY(), 50, 50);
-        }
 
-        g.setColor(Color.WHITE);
-        g.drawString("Salud: " + personaje.getSalud(), 10, 20);
-        g.drawString("Hambre: " + personaje.getHambre(), 10, 35);
-        g.drawString("Posición: (" + personaje.getX() + ", " + personaje.getY() + ")", 10, 50);
-        g.drawString("Experiencia: " + personaje.getExperiencia(), 10, 65);
+@Override
+public void paint(Graphics g) {
+    super.paint(g);
+    if (fondo != null) {
+        g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
     }
+    for (Bloque bloque : suelo) {
+        bloque.dibujar(g);
+    }
+    for (Bloque tronco : troncos) {
+        tronco.dibujar(g);
+    }
+    for (Bloque hoja : hojas) {
+        hoja.dibujar(g);
+    }
+    if (personaje.getDireccion() == 'd' && imagenDerecha != null) {
+        g.drawImage(imagenDerecha, personaje.getX(), personaje.getY(), 50, 50, this);
+    } else if (personaje.getDireccion() == 'a' && imagenIzquierda != null) {
+        g.drawImage(imagenIzquierda, personaje.getX(), personaje.getY(), 50, 50, this);
+    } else {
+        g.fillRect(personaje.getX(), personaje.getY(), 50, 50);
+    }
+    g.setColor(Color.WHITE);
+    g.drawString("Salud: " + personaje.getSalud(), 10, 20);
+    g.drawString("Hambre: " + personaje.getHambre(), 10, 35);
+    g.drawString("Posición: (" + personaje.getX() + ", " + personaje.getY() + ")", 10, 50);
+    g.drawString("Experiencia: " + personaje.getExperiencia(), 10, 65);
+}
+
+
 
     @Override
     public void keyPressed(KeyEvent e) {
